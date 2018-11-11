6d4b4df44ee2c8c7431f58b673306130c1404ab4
kohyusik <newtype26@naver.com>
2018-11-11 21:51:14 +0900

    객체 상속관계 테이블 매핑 1. Join 전략 2. Single table 전략 3. Table per class 전략

--- ch07-1-3/src/main/java/jason/jpaMain.java
+++ ch07-1-3/src/main/java/jason/jpaMain.java
@@ -1,11 +1,8 @@
 package jason;
 
-import jason.entity.Locker;
-import jason.entity.Member;
-import jason.entity.Team;
-import jason.entity.join.Album;
-import jason.entity.join.Book;
-import jason.entity.join.Movie;
+import jason.entity.table.Album;
+import jason.entity.table.Book;
+import jason.entity.table.Movie;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
@@ -17,13 +14,10 @@ public class jpaMain {
     public static void main(String[] args) {
 
         EntityManagerFactory emf = 
-                Persistence.createEntityManagerFactory("jpa-ch07");
+                Persistence.createEntityManagerFactory("jpa-ch07-1-3");
         EntityManager em = emf.createEntityManager();
         EntityTransaction tx = em.getTransaction();
 
-        /*
-        *  One-to-many unidirectional
-        * */
         try {
 
             tx.begin();
@@ -43,34 +37,6 @@ public class jpaMain {
     }
     
     private static void unidirectional(EntityManager em) {
-
-        Member member1 = new Member("koh");
-        Member member2 = new Member("jason");
-
-        Team team = new Team("Korea");
-
-        team.getMembers().add(member1);
-        team.getMembers().add(member2);
-
-        em.persist(member1);
-        em.persist(member2);
-        em.persist(team);
-
-        Locker locker = new Locker("lock-15");
-        System.out.println(">>>> persist");
-        em.persist(locker);
-        member2.setLocker(locker);
-        
-        
-        Team foundTeam = em.find(Team.class, 1l);
-        System.out.println(">>>>>> team.getName : ");
-        System.out.println(foundTeam.getName());
-
-        System.out.println(">>>>>> team.getMember : ");
-        System.out.println(foundTeam.getMembers().get(1).getName());
-
-        System.out.println(">>>>>> member.getLock : ");
-        System.out.println(foundTeam.getMembers().get(1).getLocker().getName());
 
 
         Movie movie = new Movie();
@@ -93,6 +59,5 @@ public class jpaMain {
         em.persist(movie);
         em.persist(album);
         em.persist(book);
-
     }
 }
